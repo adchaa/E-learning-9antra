c7c799e74ccceb06e86e15d0dc3b8290ee06d924
EYA MANAI <eyamanai35@outlook.com>
2023-12-29 21:44:53 +0100

    added Home Image and Navbar

--- Front/src/pages/Home/Home.jsx
+++ Front/src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Contact from "../../components/Contact/Contact";
+import HomeImg from "../../components/HomeImg/HomeImg";
+import Navbar from "../../components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
+      <HomeImg />
       <Contact />
     </div>
   );
